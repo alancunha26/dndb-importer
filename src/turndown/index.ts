7c28d0e4934c16a8238d8e954489b30f4aeb05a1
Alan Cunha <alan@alancunha.com>
2025-11-17 21:14:39 -0300

    feat: add flexible-columns list formatting

--- src/turndown/index.ts
+++ src/turndown/index.ts
@@ -6,7 +6,7 @@
 import TurndownService from "turndown";
 import { gfm } from "@truto/turndown-plugin-gfm";
 import type { MarkdownConfig } from "../types";
-import { removeHeadingLinks, unwrapLinkedImages, imageAltText, figureCaptionRule, asideRule } from "./rules";
+import { removeHeadingLinks, unwrapLinkedImages, imageAltText, figureCaptionRule, asideRule, flexibleColumnsRule } from "./rules";
 
 export function createTurndownService(
   config: MarkdownConfig,
@@ -30,6 +30,7 @@ export function createTurndownService(
   turndownService.use(imageAltText(imageMapping));
   turndownService.use(figureCaptionRule);
   turndownService.use(asideRule(config));
+  turndownService.use(flexibleColumnsRule);
 
   return turndownService;
 }
--- src/turndown/rules/index.ts
+++ src/turndown/rules/index.ts
@@ -8,6 +8,7 @@ export { unwrapLinkedImages } from "./unwrap-linked-images";
 export { imageAltText } from "./image-alt-text";
 export { figureCaptionRule } from "./figure-caption";
 export { asideRule } from "./aside";
+export { flexibleColumnsRule } from "./flexible-columns";
 
 // TODO: Add more custom rules for D&D Beyond content
 // - Stat blocks
